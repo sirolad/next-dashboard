deea296b81bc244f53a73d04307896151d44b43f
Surajudeen Akande <sirolad@gmail.com>
2024-11-13 16:35:32 +0100

    update other features

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,11 +15,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 10000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    //console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -27,6 +27,7 @@ export async function fetchRevenue() {
     throw new Error('Failed to fetch revenue data.');
   }
 }
+
 
 export async function fetchLatestInvoices() {
   try {
